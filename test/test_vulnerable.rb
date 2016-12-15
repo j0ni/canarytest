e44f94dd04b4af375af7a1cfe4267c3b9588aa7d
J Irving <j@lollyshouse.ca>
2016-12-15 17:04:07 -0500

    lol

--- test/test_vulnerable.rb
+++ test/test_vulnerable.rb
@@ -1,11 +1,11 @@
 describe "how much of our ass is in the air" do
   before do
+    puts "configured base_uri: #{Appcanary.configuration.base_uri}"
+
     Appcanary.update_monitor!
   end
 
   it "satisfies appcanary's bloody minded analysis" do
-    puts "configured base_uri: #{Appcanary.configuration.base_uri}"
-
     assert(!Appcanary.am_I_critically_fucked?, "srsly get woke")
     assert(!Appcanary.am_I_highly_fucked?, "srsly get woke")
     assert(!Appcanary.vulnerable?, "srsly get woke")
